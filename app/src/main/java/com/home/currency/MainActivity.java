e701710006cb48f3fbb8111dd2908f5a15ce0b58
mansonyu <mansonyu0204@gmail.com>
2019-01-28 13:03:28 +0800

    作業 3 為Currency專案抽取字串與多國語系

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -5,33 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText ntd;
+    private TextView jp;
+    private TextView us;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findView();
+
+    }
+
+    private void findView() {
         ntd = findViewById(R.id.ntd);
+        jp = findViewById(R.id.JP);
+        us = findViewById(R.id.US);
     }
 
     public void Currency(View view) {
         String m =  ntd.getText().toString();
         if(m.length() <= 0){
             new AlertDialog.Builder(this)
-                    .setTitle("Problem")
-                    .setMessage("Please enter you NTD amount")
+                    .setTitle(R.string.problem)
+                    .setMessage(R.string.please_enter_ntd)
+                    .setPositiveButton(R.string.ok,null)
                     .show();
         }
         else
         {
             float result = Float.parseFloat(m) / 30.9f;
             new AlertDialog.Builder(this)
-                    .setTitle("result")
-                    .setMessage("USD is " + result)
+                    .setTitle(R.string.result)
+                    .setMessage(getString(R.string.usd_id) + result)
+                    .setPositiveButton(R.string.ok, null)
                     .show();
 
         }
